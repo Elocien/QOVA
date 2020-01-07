de05ed08731ef18e8fe8a465ae802f0257fb22ce
Elocien <lucianmcintyre@gmail.com>
2020-01-07 13:13:04 +0100

    Initialised Project

---
+++ src/main/java/com/example/qova/QovaApplication.java
@@ -0,0 +1,13 @@
+package com.example.qova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QovaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/qova/QovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
