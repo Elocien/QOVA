8c0b8c6ae22066750e5d52b26e52317e184363bb
elocien <lucianmcintyre@gmail.com>
2020-04-01 14:44:22 +1000

    Controller errors, not sure how to fix constructor

--- src/main/java/qova/course/CourseController.java
+++ src/main/java/qova/course/CourseController.java
@@ -10,18 +10,18 @@ import qova.survey.SurveyManagement;
 @Controller
 public class CourseController {
 
-    // // private final CourseManagement courseManagement;
-    // private final SurveyManagement surveyManagement;
+    // private final CourseManagement courseManagement;
+    private final SurveyManagement surveyManagement;
 
-	// CourseController(SurveyManagement surveyManagement) {
-    //     // this.courseManagement = Objects.requireNonNull(courseManagement);
-    //     this.surveyManagement = surveyManagement;
-	// }
+	CourseController(SurveyManagement surveyManagement) {
+        // this.courseManagement = Objects.requireNonNull(courseManagement);
+        this.surveyManagement = surveyManagement;
+	}
 
 
     @GetMapping("/")
     public String welcome (){
-        // surveyManagement.createSurvey();
+        surveyManagement.createSurvey();
         return "home";
     }
 
