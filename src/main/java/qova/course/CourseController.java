b87c13926a9a29a89ddbc26bef4d527decfc274e
TimHerrmannTU <56600495+TimHerrmannTU@users.noreply.github.com>
2020-06-09 16:38:48 +0200

    Taking the trash out

--- src/main/java/qova/course/CourseController.java
+++ src/main/java/qova/course/CourseController.java
@@ -238,7 +238,7 @@ public class CourseController {
         model.addAttribute("coursename", course.getName());
         */
         
-        return "questioneditor4";
+        return "questioneditor";
     }
 
 
