8311b0e705dc7ff706ebe368f2dfb1449c3f7b69
elocien <lucianmcintyre@gmail.com>
2020-06-16 03:41:37 +0200

    Fixed method which retrieves defaultSurvey

--- src/main/java/qova/admin/AdminManagement.java
+++ src/main/java/qova/admin/AdminManagement.java
@@ -2,6 +2,7 @@ package qova.admin;
 
 import java.util.ArrayList;
 import java.util.Objects;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -50,7 +51,7 @@ public class AdminManagement {
 
 
     //Get Default survey from Repo
-    public DefaultSurvey getDefaultSurvey() throws Exception{
+    public String getDefaultSurvey() throws Exception{
         ArrayList<DefaultSurvey> surveyList = new ArrayList<DefaultSurvey>();
         Iterable<DefaultSurvey> iterable = repo.findAll();
 
@@ -60,14 +61,14 @@ public class AdminManagement {
 
         //default case, return the default survey
         if(  !(surveyList.isEmpty())   &&   !(surveyList.size() > 1)  ){
-            return surveyList.get(0);
+            return surveyList.get(0).getDefaultSurvey();
         }
 
         //Create default survey, if one does not exist anymore (not sure how this would happen, but better to catch all cases)
         else if(surveyList.isEmpty()){
             DefaultSurvey surv = new DefaultSurvey("[]");
             repo.save(surv);
-            return surv;
+            return surv.getDefaultSurvey();
         }
 
         //delete any excessive surveys
@@ -75,7 +76,7 @@ public class AdminManagement {
             for(int i = 1; i < surveyList.size(); i++){
                 repo.delete(surveyList.get(i));
             }
-            return surveyList.get(0);
+            return surveyList.get(0).getDefaultSurvey();
             
         } 
 
@@ -86,13 +87,33 @@ public class AdminManagement {
     }
 
 
+    private DefaultSurvey getDefaultSurveyObject(){
+
+        //There should only be one Default Survey, with id = 1; Using findAll() returns Iterable, so this is the lazy approach
+        Long i = 1L;
+
+        //retrive survey from repo
+        Optional<DefaultSurvey> defSurvey = repo.findById(i);
+
+        if(!defSurvey.isPresent()){
+            DefaultSurvey surv = new DefaultSurvey("[]");
+            repo.save(surv);
+            return surv;
+        }
+        else{
+            return defSurvey.get();
+        }
+        
+    }
+
+
 
 
 
 
     //Submission of new default survey
     public void updateDefaultSurvey(SurveyForm form) throws Exception {
-        getDefaultSurvey().setDefaultSurvey(form.getQuestionnairejson());
+        getDefaultSurveyObject().setDefaultSurvey(form.getQuestionnairejson());
     }
 
 
