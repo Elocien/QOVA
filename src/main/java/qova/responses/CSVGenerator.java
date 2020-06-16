5e7cd08598e6d392c182c74476b369ba10d7b767
elocien <lucianmcintyre@gmail.com>
2020-06-16 06:55:17 +0200

    Started on CSV Writer, waiting for formatting info from Nora

---
+++ src/main/java/qova/responses/CSVGenerator.java
@@ -0,0 +1,99 @@
+package qova.responses;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.itextpdf.io.IOException;
+import com.opencsv.CSVWriter;
+
+public class CSVGenerator {
+    
+    public byte[] createCSV(ArrayList<Response> allResponses) throws java.io.IOException {
+        
+           
+        //Output Stream
+        var stream = new ByteArrayOutputStream();
+
+        //Output Stream Writer
+        var outpuStreamWriter = new OutputStreamWriter(stream);
+
+        // CSVWriter, OutputStreamWriter object as parameter 
+        CSVWriter writer = new CSVWriter(outpuStreamWriter);
+
+
+
+
+        //Map that contains responses/ The key is the position of response objects in ArrayList
+            Map<Integer, ArrayList<Response>> responses = new HashMap<Integer, ArrayList<Response>>();
+
+        //Iterate through ArrayList and add each response to the correct ArrayList of the HashMap. The key of the 
+        for(int i = 0; i < allResponses.size(); i++){
+            Response rsp = allResponses.get(i);
+            Integer pos = rsp.getPosition();
+            
+            //temporary List which holds the Responses for a given key in the map
+            ArrayList<Response> tempList = responses.get(pos);
+
+            // if list does not exist create it
+            if(tempList == null) {
+                tempList = new ArrayList<Response>();
+                tempList.add(rsp);
+                responses.put(pos, tempList);
+            } else {
+                // add if item is not already in list
+                if(!tempList.contains(rsp)){
+                    tempList.add(rsp);
+                } 
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // CSV Standard Header (The options for all question types must be added after)
+        String[] header = { "Course Name", "Instance", "Semster of Students", "Course Type", " Submission Date and Time", "Question", "Response Type", "Class No.", "Text Response", "Binary Answer" }; 
+        writer.writeNext(header); 
+
+        // add data to csv 
+        String[] data1 = { "Aman", "10", "620" }; 
+        writer.writeNext(data1); 
+        String[] data2 = { "Suraj", "10", "630" }; 
+        writer.writeNext(data2); 
+
+        // closing writer connection 
+        writer.close(); 
+        
+
+
+        return stream.toByteArray();
+    }
+
+    
+}
