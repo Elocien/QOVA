72e28a3ffefac448d4c27876f5a6f7d2a7e8b986
elocien <lucianmcintyre@gmail.com>
2020-06-18 07:01:59 +0200

    Nearly finished CSV Gen, might need changes to response Object, so will resume later

--- src/main/java/qova/responses/CSVGenerator.java
+++ src/main/java/qova/responses/CSVGenerator.java
@@ -3,16 +3,53 @@ package qova.responses;
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 import com.opencsv.CSVWriter;
 
+import qova.course.Course;
+
 public class CSVGenerator {
     
     public byte[] createCSV(ArrayList<Response> allResponses) throws java.io.IOException {
         
-           
+        //Initialise Data Structures
+
+        //Response Attributes
+        //We use the Response at position 0, because it has the same Course and CourseType as all other Responses given. See ResponseManagement for details on the retrieval of Responses for generation
+        Response response = allResponses.get(0);
+        String courseType = String.valueOf(response.getCourseType());
+        String question = response.getQuestion();
+        String classNo = String.valueOf(response.getClassNo());
+        
+
+        //Course Specific Attributes
+        Course course = response.getCourse();
+        String courseName = course.getName();
+        String courseInstanceSemesterForm = course.getSemesterUI();
+        String semesterOfStudents = String.valueOf(course.getSemesterOfStudents());
+        
+
+
+
+
+
+        // CSV Standard Header (The options for all question types must be added after)
+        ArrayList<String> header = new ArrayList<String>( Arrays.asList("Course Name", "Instance", "Semster of Students", "Course Type", "Question", "Class No."));
+ 
+        ArrayList<String> data = new ArrayList<String>( Arrays.asList( courseName, courseInstanceSemesterForm, semesterOfStudents, courseType, question, classNo));
+
+
+
+
+
+
+
+        //setup CSVWriter and Stream
+        //----------------------------------------------------------------------------------------------------------
+
         //Output Stream
         var stream = new ByteArrayOutputStream();
 
@@ -21,6 +58,9 @@ public class CSVGenerator {
 
         // CSVWriter, OutputStreamWriter object as parameter 
         CSVWriter writer = new CSVWriter(outpuStreamWriter);
+
+
+        //----------------------------------------------------------------------------------------------------------
 
 
 
@@ -58,6 +98,8 @@ public class CSVGenerator {
 
 
 
+        
+        
 
 
 
@@ -70,17 +112,51 @@ public class CSVGenerator {
 
 
 
+        //Step 2:
+        //Main Loop; iterate through all responses
+        for(int pos = 0; pos < responses.size(); pos++){
+
+            //ArrayList of responses for the current Position. These will be from the same Course and have the same: CourseType, classNo, ResponseType and position
+            ArrayList<Response> responsesForPos = responses.get(pos);
+
+            //Get ResponseType for Responses of given position (pos). We assume this to be the same for every Response of that position (if error occurs, check serialisation of Responses)
+            ResponseType responseType = responsesForPos.get(0).getResponseType();
 
 
-        // CSV Standard Header (The options for all question types must be added after)
-        String[] header = { "Course Name", "Instance", "Semster of Students", "Course Type", " Submission Date and Time", "Question", "Response Type", "Class No.", "Text Response", "Binary Answer" }; 
+            //Multiple Choice or Single Choice
+            if(responseType.equals(ResponseType.MULTIPLE_CHOICE) || responseType.equals(ResponseType.SINGLE_CHOICE)){
+                
+                //passes the options given to users, for a single Multiple Choice or Single Choice question, to the method, which appends the header with these
+                header.addAll(responseMultipleChoiceAndSingleChoiceHeader(responsesForPos.get(0).getOptionsMCDD()));
+
+                //passes all responses at the current position to the method, which accumulates them and addes them to the data 
+            }
+
+            else if(responseType == ResponseType.TEXT_RESPONSE){
+
+            }
+
+            else if(responseType == ResponseType.BINARY_ANSWER){
+
+            }
+
+            else{
+
+            }
+        }
+
+
+        //Write Header
+        //convert ArrayList<String> header to String[]
         writer.writeNext(header); 
+       
 
-        // add data to csv 
-        String[] data1 = { "Aman", "10", "620" }; 
-        writer.writeNext(data1); 
-        String[] data2 = { "Suraj", "10", "630" }; 
-        writer.writeNext(data2); 
+        //Write Data
+        //convert ArrayList<String> data to String[]
+        writer.writeNext(data); 
+        
+
+
 
         // closing writer connection 
         writer.close(); 
@@ -90,5 +166,47 @@ public class CSVGenerator {
         return stream.toByteArray();
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * 
+     * @return an ArrayList containing new header elements for Multiple Choice and Single Choice Responses at a set position
+     */
+    public ArrayList<String> responseMultipleChoiceAndSingleChoiceHeader(ArrayList<String> options){
+        
+        //ArrayList containing options presented to the viewer in the questionaire
+        
+
+
+
+
+        return 
+    }
+
+
+
+    /**
+     * 
+     * @return an ArrayList of data elements for Multiple Choice and Single Choice Responses at a set position
+     */
+    public ArrayList<String> responseMultipleChoiceAndSingleChoiceData(){
+        //ArrayList containing all column titles 
+        ArrayList<String> columnTitles = responsesForPos.get(0).getOptionsMCDD();
+
+
+
+
+        return 
+    }
     
 }
