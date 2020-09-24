d948d32c51ae7cef69d3e375d323591bec4afaf0
Elocien <lucianmcintyre@gmail.com>
2020-09-24 15:39:39 +0200

    Removed unused Id generator

--- src/main/java/qova/objects/Course.java
+++ src/main/java/qova/objects/Course.java
@@ -6,17 +6,14 @@ import java.util.UUID;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
 //id-generator imports
 import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Parameter;
 
 import qova.enums.CourseFaculty;
 import qova.enums.CourseType;
-import qova.IdGenerator;
 
 
 
