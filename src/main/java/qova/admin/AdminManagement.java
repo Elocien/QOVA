123f21362d7494c7163598ea0333bbc9ca9f063b
Elocien <lucianmcintyre@gmail.com>
2020-09-28 19:09:43 +0200

    Removed useless string and import

--- src/main/java/qova/admin/AdminManagement.java
+++ src/main/java/qova/admin/AdminManagement.java
@@ -1,7 +1,6 @@
 package qova.admin;
 
 import java.util.Objects;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,58 +9,43 @@ import org.springframework.transaction.annotation.Transactional;
 import qova.enums.CourseType;
 import qova.forms.SurveyForm;
 
-
 @Service
 @Transactional
 public class AdminManagement {
-    
+
     private final DefaultSurveyRepository repo;
 
-    private String defaultSurveyString = "[{\"type\":\"YesNo\",\"question\":\"Scheint die Sonne\"},{\"type\":\"MultipleChoice\",\"question\":\"Wie geht es dir?\",\"answers\":[\"Okay\",\"Gut\",\"Schlecht\"]},{\"type\":\"SingleChoice\",\"question\":\"Ja Neine Vielleicht\",\"answers\":[\"Ja\",\"Nein\",\"Vielleicht\"]},{\"type\":\"FreeText\",\"question\":\"STONKS\"}]";
-
     /**
-     * The Constructor checks if the {@linkplain DefaultSurvey} is present and persisted; if it is not, a new one is created;
+     * The Constructor checks if the {@linkplain DefaultSurvey} is present and
+     * persisted; if it is not, a new one is created;
      * 
-     * @param repo The {@linkplain DefaultSurveyRepository}. Contains the single instance of the defaultSurvey.
+     * @param repo The {@linkplain DefaultSurveyRepository}. Contains the single
+     *             instance of the defaultSurvey.
      */
     @Autowired
     public AdminManagement(DefaultSurveyRepository repo) {
         this.repo = Objects.requireNonNull(repo);
     }
 
-
-
-    public String concatenateDefaultSurveyToSurveyString(String surveyJson, CourseType type){
+    public String concatenateDefaultSurveyToSurveyString(String surveyJson, CourseType type) {
         String defaultSurvey = getDefaultSurvey(type);
 
-        return (defaultSurvey.substring(0, defaultSurvey.length()-1) + "," + surveyJson.substring(1));
+        return (defaultSurvey.substring(0, defaultSurvey.length() - 1) + "," + surveyJson.substring(1));
     }
 
-
-
-
-
-
-
-    //Get Default survey from Repo
+    // Get Default survey from Repo
     public String getDefaultSurvey(CourseType type) {
         return repo.findDefaultSurveyForType(type).getDefaultSurveyJson();
     }
 
-
-    //Get the DefaultSurvey Object from the repo
-    private DefaultSurvey getDefaultSurveyObject(CourseType type){
+    // Get the DefaultSurvey Object from the repo
+    private DefaultSurvey getDefaultSurveyObject(CourseType type) {
         return repo.findDefaultSurveyForType(type);
     }
 
-
-
-    //Submission of new default survey
+    // Submission of new default survey
     public void updateDefaultSurvey(SurveyForm form, CourseType type) {
         getDefaultSurveyObject(type).setDefaultSurveyJson(form.getQuestionnairejson());
     }
 
 }
-
-
-
