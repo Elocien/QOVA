ca970b2735997859add857fb5f93ca3fc114030b
Elocien <lucianmcintyre@gmail.com>
2020-10-01 19:28:41 +0200

    Fixed responseTest to adjust for new AbstractResponse

--- src/test/java/qova/response/responseTest.java
+++ src/test/java/qova/response/responseTest.java
@@ -1,11 +1,10 @@
 package qova.response;
 
 import qova.AbstractIntegrationTest;
-import qova.enums.CourseFaculty;
 import qova.enums.CourseType;
+import qova.objects.AbstractResponse;
 import qova.objects.BinaryResponse;
 import qova.objects.Course;
-import qova.objects.CourseInstance;
 import qova.objects.MultipleChoiceResponse;
 import qova.objects.SingleChoiceResponse;
 import qova.objects.SurveyResponse;
@@ -13,9 +12,7 @@ import qova.objects.TextResponse;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -27,44 +24,17 @@ public class responseTest extends AbstractIntegrationTest {
     @Test
     public void UserResponseConstructorTest() throws Exception {
 
-        var name = "Rechnernetze";
-
-        List<String> lectureTitles = new ArrayList<>();
-        lectureTitles.addAll(
-                Arrays.asList("Einführung", "Bitübertragungsschicht", "Netztechnologien 1", "Netztechnologien 2",
-                        "Sicherungsschicht", "Vermittlungsschicht", "Transportschicht", "Netzwerkperformance",
-                        "Internetdienste", "Multimediakommunikation", "Mobile Computing", "Verteilte Systeme"));
-        var lecture = new CourseInstance(CourseType.LECTURE, 1, 12, lectureTitles, true);
-
-        List<String> tutorialTitles = new ArrayList<>();
-        tutorialTitles.addAll(
-                Arrays.asList("Einführung", "Bitübertragungsschicht", "Netztechnologien 1", "Netztechnologien 2",
-                        "Sicherungsschicht", "Vermittlungsschicht", "Transportschicht", "Netzwerkperformance",
-                        "Internetdienste", "Multimediakommunikation", "Mobile Computing", "Verteilte Systeme"));
-        var tutorial = new CourseInstance(CourseType.TUTORIAL, 8, 12, tutorialTitles, true);
-
-        CourseInstance seminar = null;
-
-        CourseInstance practical = null;
-
-        var semesterOfStudents = 4;
-        var faculty = CourseFaculty.COMPUTER_SCIENCE;
-        var courseDate = LocalDate.of(2020, 10, 4);
-        var semesterString = "SoSe 2020";
-
-        Course crs = new Course(name, lecture, tutorial, seminar, practical, semesterOfStudents, faculty,
-                semesterString, courseDate);
-
         // ----------------------------------------------------------------------------------------------------------------------
+        Course course = Mockito.mock(Course.class);
 
         var type = CourseType.LECTURE;
         var instance = 1;
         var group = 1;
 
-        SurveyResponse response = new SurveyResponse(crs, type, instance, group);
+        List<AbstractResponse> listOfResponses = new ArrayList<>();
 
-        BinaryResponse bnr = new BinaryResponse(response,
-                "Would you consider recommending the lecture to other students?", 0);
+        var binaryResponseQuestion = "Would you consider recommending the lecture to other students?";
+        BinaryResponse bnr = new BinaryResponse(binaryResponseQuestion, 0, true);
         for (int i = 0; i < 35; i++) {
             bnr.incrementYes();
         }
@@ -72,14 +42,16 @@ public class responseTest extends AbstractIntegrationTest {
             bnr.incrementNo();
         }
 
+        listOfResponses.add(bnr);
+
         List<String> mcOptions = new ArrayList<>();
         mcOptions.add("1");
         mcOptions.add("2");
         mcOptions.add("3");
         mcOptions.add("4");
         mcOptions.add("5");
-        MultipleChoiceResponse mcr = new MultipleChoiceResponse(response,
-                "From 1 to 5, what would you rate the lecture?", 1, mcOptions);
+        var multipleChoiceResponseQuestion = "From 1 to 5, what would you rate the lecture?";
+        MultipleChoiceResponse mcr = new MultipleChoiceResponse(multipleChoiceResponseQuestion, 1, mcOptions, true);
 
         List<Integer> mcAnswers1 = new ArrayList<>();
         mcAnswers1.add(0);
@@ -104,7 +76,8 @@ public class responseTest extends AbstractIntegrationTest {
             mcr.incrementTotals(mcAnswers3);
         }
 
-        TextResponse txr = new TextResponse(response, "What is your opinion of the lecture, is it helpful?", 2);
+        var textResponseQuestion = "What is your opinion of the lecture, is it helpful?";
+        TextResponse txr = new TextResponse(textResponseQuestion, 2, false);
         for (int i = 0; i < 20; i++) {
             txr.addTextSubmission("this is a bit of a test");
         }
@@ -119,6 +92,8 @@ public class responseTest extends AbstractIntegrationTest {
                     "this is a very very very very very very very very very very very very very very very very very very very very large test");
         }
 
+        SurveyResponse response = new SurveyResponse(course, type, instance, group, listOfResponses);
+
         List<String> listOfStudentIds = new ArrayList<>();
         for (int i = 0; i < 50; i++) {
             String id = UUID.randomUUID().toString();
@@ -126,7 +101,7 @@ public class responseTest extends AbstractIntegrationTest {
             listOfStudentIds.add(id);
         }
 
-        assertEquals(crs, response.getCourse());
+        assertEquals(course, response.getCourse());
         assertEquals(type, response.getCourseType());
         assertEquals(group, response.getGroupNumber());
         assertEquals(instance, response.getInstanceNumber());
@@ -134,16 +109,17 @@ public class responseTest extends AbstractIntegrationTest {
         assertEquals(listOfStudentIds.containsAll(response.getListOfStudentsThatSubmitted()),
                 response.getListOfStudentsThatSubmitted().containsAll(listOfStudentIds));
         assertEquals(50, response.getNumberOfSubmissions());
+        assertEquals(listOfResponses, response.getListOfResponses());
     }
 
     @Test
     public void BinaryResponseConstructorTest() {
 
-        SurveyResponse surveyResponse = Mockito.mock(SurveyResponse.class);
-
         var question = "Was the lecture informative";
+        var surveyPosition = 1;
+        var isDefaultQuestion = true;
 
-        BinaryResponse br = new BinaryResponse(surveyResponse, question, 0);
+        BinaryResponse br = new BinaryResponse(question, surveyPosition, isDefaultQuestion);
         br.incrementYes();
         br.incrementNo();
         br.incrementNo();
@@ -155,31 +131,31 @@ public class responseTest extends AbstractIntegrationTest {
         assertEquals(2, br.getNoTotal());
         assertEquals(3, br.getTotal());
         assertEquals(qova.enums.ResponseType.BINARY_ANSWER, br.getType());
-        assertEquals(0, br.getSurveyPosition());
-        assertEquals(surveyResponse, br.getSurveyResponse());
+        assertEquals(surveyPosition, br.getSurveyPosition());
+        assertEquals(isDefaultQuestion, br.getIsDefaultQuestion());
     }
 
     @Test
     public void TextResponseConstructorTest() {
 
-        SurveyResponse surveyResponse = Mockito.mock(SurveyResponse.class);
-
         var question = "Was the lecture informative";
+        var surveyPosition = 10;
+        var isDefaultQuestion = false;
 
-        TextResponse tr = new TextResponse(surveyResponse, question, 1);
+        TextResponse tr = new TextResponse(question, surveyPosition, isDefaultQuestion);
 
         assertEquals(question, tr.getQuestion());
         assertEquals(qova.enums.ResponseType.TEXT_RESPONSE, tr.getType());
-        assertEquals(1, tr.getSurveyPosition());
-        assertEquals(surveyResponse, tr.getSurveyResponse());
+        assertEquals(surveyPosition, tr.getSurveyPosition());
+        assertEquals(isDefaultQuestion, tr.getIsDefaultQuestion());
     }
 
     @Test
     public void SingleChoiceConstructorTest() throws Exception {
 
-        SurveyResponse surveyResponse = Mockito.mock(SurveyResponse.class);
-
         String question = "Rate the lecutre from 1 to 5";
+        var surveyPosition = 5;
+        var isDefaultQuestion = false;
 
         ArrayList<String> scOptions = new ArrayList<String>();
         scOptions.add("1");
@@ -187,7 +163,7 @@ public class responseTest extends AbstractIntegrationTest {
         scOptions.add("3");
         scOptions.add("4");
         scOptions.add("5");
-        SingleChoiceResponse scr = new SingleChoiceResponse(surveyResponse, question, 2, scOptions);
+        SingleChoiceResponse scr = new SingleChoiceResponse(question, surveyPosition, scOptions, isDefaultQuestion);
 
         for (int i = 0; i < 3; i++) {
             scr.incrementTotal(0);
@@ -216,16 +192,16 @@ public class responseTest extends AbstractIntegrationTest {
         assertEquals(scOptions, scr.getSingleChoiceOptions());
         assertEquals(totals, scr.getSingleChoiceAnswers());
         assertEquals(qova.enums.ResponseType.SINGLE_CHOICE, scr.getType());
-        assertEquals(2, scr.getSurveyPosition());
-        assertEquals(surveyResponse, scr.getSurveyResponse());
+        assertEquals(surveyPosition, scr.getSurveyPosition());
+        assertEquals(isDefaultQuestion, scr.getIsDefaultQuestion());
     }
 
     @Test
     public void MultipleChoiceConstructorTest() throws Exception {
 
-        SurveyResponse surveyResponse = Mockito.mock(SurveyResponse.class);
-
         String question = "What was good about the lecture (multiple options can be selected)";
+        var surveyPosition = 5;
+        var isDefaultQuestion = false;
 
         ArrayList<String> mcOptions = new ArrayList<String>();
         mcOptions.add("It was informative");
@@ -233,7 +209,7 @@ public class responseTest extends AbstractIntegrationTest {
         mcOptions.add("I learned something new");
         mcOptions.add("I enjoyed attending the lecture");
         mcOptions.add("I would recommend the lecture to others");
-        MultipleChoiceResponse mcr = new MultipleChoiceResponse(surveyResponse, question, 0, mcOptions);
+        MultipleChoiceResponse mcr = new MultipleChoiceResponse(question, surveyPosition, mcOptions, isDefaultQuestion);
 
         ArrayList<Integer> mcAnswers1 = new ArrayList<Integer>();
         mcAnswers1.add(0);
@@ -272,8 +248,8 @@ public class responseTest extends AbstractIntegrationTest {
         assertEquals(totals, mcr.getMultipleChoiceAnswers());
         assertEquals(5, mcr.getNumberOfOptions());
         assertEquals(qova.enums.ResponseType.MULTIPLE_CHOICE, mcr.getType());
-        assertEquals(0, mcr.getSurveyPosition());
-        assertEquals(surveyResponse, mcr.getSurveyResponse());
+        assertEquals(surveyPosition, mcr.getSurveyPosition());
+        assertEquals(isDefaultQuestion, mcr.getIsDefaultQuestion());
     }
 
 }
