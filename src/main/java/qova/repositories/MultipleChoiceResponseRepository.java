6257d3a9734428bc97c88bed80d8cccd34913c3d
Elocien <lucianmcintyre@gmail.com>
2020-10-01 19:29:15 +0200

    Removed query methods that searched by SurveyResponse, as this field was removed from the Responses

--- src/main/java/qova/repositories/MultipleChoiceResponseRepository.java
+++ src/main/java/qova/repositories/MultipleChoiceResponseRepository.java
@@ -10,10 +10,6 @@ import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 @Repository
-public interface MultipleChoiceResponseRepository extends CrudRepository <MultipleChoiceResponse, Long> {
-    
-    public Optional<MultipleChoiceResponse> findBySurveyResponseAndSurveyPosition(SurveyResponse surveyResponse, Integer surveyPosition);
-
-    public Iterable<MultipleChoiceResponse> findBySurveyResponse(SurveyResponse surveyResponse);
+public interface MultipleChoiceResponseRepository extends CrudRepository<MultipleChoiceResponse, Long> {
 
 }
--- src/main/java/qova/repositories/SingleChoiceResponseRepository.java
+++ src/main/java/qova/repositories/SingleChoiceResponseRepository.java
@@ -10,10 +10,6 @@ import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 @Repository
-public interface SingleChoiceResponseRepository extends CrudRepository <SingleChoiceResponse, Long> {
-    
-    public Optional<SingleChoiceResponse> findBySurveyResponseAndSurveyPosition(SurveyResponse surveyResponse, Integer surveyPosition);
-
-    public Iterable<SingleChoiceResponse> findBySurveyResponse(SurveyResponse surveyResponse);
+public interface SingleChoiceResponseRepository extends CrudRepository<SingleChoiceResponse, Long> {
 
 }
