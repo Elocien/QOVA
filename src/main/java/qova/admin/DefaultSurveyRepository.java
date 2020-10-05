e0ce41f9794d2b47d54ff781ba7f345d7ca86d3d
Elocien <lucianmcintyre@gmail.com>
2020-10-05 10:42:38 +0200

    Fixed default survey Id's

--- src/main/java/qova/admin/DefaultSurveyRepository.java
+++ src/main/java/qova/admin/DefaultSurveyRepository.java
@@ -9,82 +9,75 @@ import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 @Repository
-public interface DefaultSurveyRepository extends CrudRepository <DefaultSurvey, Long> {
+public interface DefaultSurveyRepository extends CrudRepository<DefaultSurvey, Long> {
 
     public static final Long lectureDefaultSurveyId = 1L;
     public static final Long tutorialDefaultSurveyId = 2L;
     public static final Long seminarDefaultSurveyId = 3L;
     public static final Long practicalDefaultSurveyId = 4L;
 
-
     default DefaultSurvey findLectureSurvey() {
         Optional<DefaultSurvey> lecture = findById(1L);
-        if(lecture.isEmpty()){
+        if (lecture.isEmpty()) {
             DefaultSurvey lectureDefaultSurvey = new DefaultSurvey(1L, CourseType.LECTURE);
             save(lectureDefaultSurvey);
             return lectureDefaultSurvey;
-        }
-        else {
+        } else {
             return lecture.get();
         }
-        
+
     }
 
     default DefaultSurvey findTutorialSurvey() {
-        Optional<DefaultSurvey> tutorial = findById(1L);
-        if(tutorial.isEmpty()){
-            DefaultSurvey tutorialDefaultSurvey = new DefaultSurvey(1L, CourseType.TUTORIAL);
+        Optional<DefaultSurvey> tutorial = findById(2L);
+        if (tutorial.isEmpty()) {
+            DefaultSurvey tutorialDefaultSurvey = new DefaultSurvey(2L, CourseType.TUTORIAL);
             save(tutorialDefaultSurvey);
             return tutorialDefaultSurvey;
-        }
-        else {
+        } else {
             return tutorial.get();
         }
-        
+
     }
 
     default DefaultSurvey findSeminarSurvey() {
-        Optional<DefaultSurvey> seminar = findById(1L);
-        if(seminar.isEmpty()){
-            DefaultSurvey seminarDefaultSurvey = new DefaultSurvey(1L, CourseType.SEMINAR);
+        Optional<DefaultSurvey> seminar = findById(3L);
+        if (seminar.isEmpty()) {
+            DefaultSurvey seminarDefaultSurvey = new DefaultSurvey(3L, CourseType.SEMINAR);
             save(seminarDefaultSurvey);
             return seminarDefaultSurvey;
-        }
-        else {
+        } else {
             return seminar.get();
         }
-        
+
     }
 
     default DefaultSurvey findPracticalSurvey() {
-        Optional<DefaultSurvey> practical = findById(1L);
-        if(practical.isEmpty()){
-            DefaultSurvey practicalDefaultSurvey = new DefaultSurvey(1L, CourseType.PRACTICAL);
+        Optional<DefaultSurvey> practical = findById(4L);
+        if (practical.isEmpty()) {
+            DefaultSurvey practicalDefaultSurvey = new DefaultSurvey(4L, CourseType.PRACTICAL);
             save(practicalDefaultSurvey);
             return practicalDefaultSurvey;
-        }
-        else {
+        } else {
             return practical.get();
         }
-        
-    }
 
-    
+    }
 
     default DefaultSurvey findDefaultSurveyForType(CourseType courseType) {
         switch (courseType) {
             case LECTURE:
                 return findLectureSurvey();
-        
+
             case TUTORIAL:
                 return findTutorialSurvey();
-        
+
             case SEMINAR:
                 return findSeminarSurvey();
-        
+
             case PRACTICAL:
                 return findPracticalSurvey();
-        
+
             default:
                 return null;
         }
