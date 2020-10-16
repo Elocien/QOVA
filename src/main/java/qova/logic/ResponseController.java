f21d226dcd3880f920f726d800a3651edf3140ad
Smurfdaddy <48996293+Smurfdaddy@users.noreply.github.com>
2020-10-16 16:03:46 +0200

    Revert "Fixed a link in the mapping for surveySelect.html"
    This reverts commit e334fba469404163b522d205be4a1a1540a2a066.

--- src/main/java/qova/logic/ResponseController.java
+++ src/main/java/qova/logic/ResponseController.java
@@ -127,7 +127,7 @@ public class ResponseController {
                 return "redirect:/survey?id="+id+"&type="+type+"&instance="+form.getInstance()+"&group="+form.getGroup();
             }
             else if (mode.equals("results")) {
-                return "redirect:/surveyResults?id="+id+"&type="+type+"&instance="+form.getInstance()+"&group="+form.getGroup();
+                return "redirect:/survey?id="+id+"&type="+type+"&instance="+form.getInstance()+"&group="+form.getGroup();
             }
             else {
                 return "error";
