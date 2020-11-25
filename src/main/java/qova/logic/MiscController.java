a30a778db4f56c83a79990eb9433466431b621d5
Elocien <lucianmcintyre@gmail.com>
2020-11-25 16:00:09 +0100

    Added privacy Policy Pdf

--- src/main/java/qova/logic/MiscController.java
+++ src/main/java/qova/logic/MiscController.java
@@ -1,19 +1,38 @@
 package qova.logic;
 
+import com.google.zxing.WriterException;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import qova.objects.Course;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Optional;
+import java.util.UUID;
 
 @Controller
 public class MiscController {
 
-
+    /**
+     * Landing Page for the application
+     * @return home.html template
+     */
     @GetMapping("/")
     public String welcome() {
         return "home";
     }
 
+    /**
+     * Landing Page for the application, identical to {@linkplain MiscController#welcome()}
+     * @return home.html template
+     */
     @GetMapping("/home")
     public String home() {
         return "home";
@@ -24,6 +43,28 @@ public class MiscController {
         session.invalidate();
 
         return "redirect:/";
+    }
+
+    @GetMapping("/privacy/download")
+    public HttpEntity<byte[]> qrcode(HttpServletResponse response, @RequestParam String type, @RequestParam UUID id)
+            throws IOException, WriterException {
+
+
+        // generate filename
+        String filename = "privacyPolicy.pdf";
+
+        // Get PDF
+        File file = new File("/resources/static/resources/privacyPolicy.pdf");
+        byte[] pdf = Files.readAllBytes(file.toPath());
+
+
+        // Set HTTP headers and return HttpEntity
+        HttpHeaders header = new HttpHeaders();
+        header.setContentType(MediaType.APPLICATION_PDF);
+        header.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + filename);
+        header.setContentLength(pdf.length);
+
+        return new HttpEntity<>(pdf, header);
     }
 
 
