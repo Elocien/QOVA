aed8c6139593d508c065d0aa75e33665e8c85f39
Elocien <lucianmcintyre@gmail.com>
2021-04-24 14:06:47 +0200

    Authentication test

--- src/main/java/qova/logic/MiscController.java
+++ src/main/java/qova/logic/MiscController.java
@@ -1,47 +1,59 @@
 package qova.logic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import qova.users.CurrentUserDetails;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.Objects;
 
 @Controller
 public class MiscController {
+
+    @Autowired
+    private final CourseManagement courseManagement;
+
+    @Autowired
+    MiscController(CourseManagement courseManagement){
+        this.courseManagement = Objects.requireNonNull(courseManagement);
+    }
 
     /**
      * Landing Page for the application
      * @return home.html template
      */
     @GetMapping("/")
-    public String welcome() {
+    public String welcome(Model model, @AuthenticationPrincipal CurrentUserDetails userDetails, HttpServletRequest request) {
+        String userId = userDetails.getUsername();
+        if(request.isUserInRole("ROLE_STAFF")){
+            System.out.println("lol");
+            model.addAttribute("courseList", courseManagement.findByOwnerid(userId));
+        }
         return "home";
     }
 
-    /**
-     * Landing Page for the application, identical to {@linkplain MiscController#welcome()}
-     * @return home.html template
-     */
-    @GetMapping("/home")
-    public String home() {
-        return "home";
-    }
 
     /**
-     * Landing Page for the application, identical to {@linkplain MiscController#welcome()}.
+     * Landing url for the application when a user logs in. After authentication, redirect to home page
      * This is used so that users enter a directory where authentication is required, thus triggering shibboleth.
      * @return home.html template
      */
     @GetMapping("survey/home")
     public String loginHome() {
-        return "home";
+        return "redirect:/";
     }
 
 
